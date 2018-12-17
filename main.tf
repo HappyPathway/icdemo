17a5045df0619884ee1957567d686035209840ac
David Arnold <dja+djaboxx@sonic.net>
2018-12-17 15:47:15 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------------
+// Workspace Data
+data "terraform_remote_state" "awsdemo_darnold_tfe_network" {
+  backend = "atlas"
+  config {
+    address = "app.terraform.io"
+    name    = "AWSDemoDarnoldTFE/Network"
+  }
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "ec2_instance" {
+  source  = "app.terraform.io/AWSDemoDarnoldTFE/ec2-instance/aws"
+  version = "4.0.0"
+
+  instance_name = "InstaCartDemo"
+  instance_type = "i2.2xlarge"
+  key_name = "tfe-demos-darnold"
+  subnet_id = "${element(data.terraform_remote_state.awsdemo_darnold_tfe_network.public_subnets, 0)}"
+}
